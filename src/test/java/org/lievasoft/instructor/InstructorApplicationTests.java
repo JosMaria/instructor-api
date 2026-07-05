16f2bfb1441137b95da1cbde8dc45a402514c043
Jose Maria <josmariaguilar@gmail.com>
2026-07-05 19:09:02 -0400

    Initial commit

---
+++ src/test/java/org/lievasoft/instructor/InstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lievasoft.instructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
