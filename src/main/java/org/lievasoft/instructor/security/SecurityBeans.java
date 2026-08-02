5abf420fd8d9425d8618614a3ce85cda0897a846
Jose Maria <josmariaguilar@gmail.com>
2026-08-02 19:49:44 -0400

    refactor: move security beans

---
+++ src/main/java/org/lievasoft/instructor/security/SecurityBeans.java
@@ -0,0 +1,27 @@
+package org.lievasoft.instructor.security;
+
+import org.lievasoft.instructor.repository.AccountRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class SecurityBeans {
+
+	@Bean
+	public UserDetailsService userDetailsService(AccountRepository accountRepository) {
+		return username -> {
+			var errorMsg = "Account with username %s does not exist".formatted(username);
+			return accountRepository.findByUsername(username)
+					.orElseThrow(() -> new UsernameNotFoundException(errorMsg));
+		};
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+	}
+}
--- src/main/java/org/lievasoft/instructor/security/SecurityConfig.java
+++ src/main/java/org/lievasoft/instructor/security/SecurityConfig.java
@@ -1,13 +1,11 @@
-package org.lievasoft.instructor.config;
+package org.lievasoft.instructor.security;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,19 +13,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(
-			HttpSecurity http, @Qualifier("defaultUserDetailsService") UserDetailsService userDetailsService) {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 		return http
 				.csrf(AbstractHttpConfigurer::disable)
-				.userDetailsService(userDetailsService)
 				.authorizeHttpRequests(authorize -> authorize
 						.requestMatchers("/actuator/**").permitAll()
 						.requestMatchers("/api/v1/locutions").hasRole("ADMIN")
 						.requestMatchers("/api/v1/accounts/register").permitAll()
 						.anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
-				.oauth2Login(Customizer.withDefaults())
-				.formLogin(Customizer.withDefaults())
 				.build();
 	}
 }
